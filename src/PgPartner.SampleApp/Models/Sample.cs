e742375535dd91c2cec385afdecd34cb1aa84b78
oleg3790 <oleg.krysko@outlook.com>
2021-10-03 21:08:54 -0500

    Scaffold everything out

---
+++ src/PgPartner.SampleApp/Models/Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PgPartner.SampleApp.Models
+{
+    public class Sample
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ItemSum { get; set; }
+
+        public decimal ItemAmount { get; set;  }
+    }
+}
