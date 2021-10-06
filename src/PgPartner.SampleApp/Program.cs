702318daf3cbb5939ff5f5039b55da33fa602213
oleg3790 <oleg.krysko@outlook.com>
2021-10-06 00:20:14 -0500

    Add implementation

--- src/PgPartner.SampleApp/Program.cs
+++ src/PgPartner.SampleApp/Program.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Npgsql;
+using NpgsqlTypes;
+using PgPartner.SampleApp.Models;
+using System;
+using System.Collections.Generic;
 
 namespace PgPartner.SampleApp
 {
     class Program
     {
+        private static string CreateSamplesTable = @"
+            create extension if not exists ""uuid-ossp"";
+
+            create table public.samples (
+                id          uuid default uuid_generate_v4(),
+                name        varchar(200) not null,
+                sum         int null,
+                amount      decimal null
+            );";
+
+        private static string QueryForSamplesTable = @"
+            select *
+            from information_schema.tables
+            where table_schema = 'public'
+            and table_name = 'samples'";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=test; Username=postgres; Password=admin;");
+            conn.Open();
+
+            var queryCommand = new NpgsqlCommand(QueryForSamplesTable, conn);
+            var tableDoesNotExist = queryCommand.ExecuteScalar() == null;
+
+            if (tableDoesNotExist)
+            {
+                var createSampleTableCommand = new NpgsqlCommand(CreateSamplesTable, conn);
+                createSampleTableCommand.ExecuteNonQuery();
+            }
+
+            var samples = new List<Sample>()
+            {
+                new Sample { Id = Guid.NewGuid(), Name = "Test", ItemSum = 200, ItemAmount = 10 },
+                new Sample { Id = Guid.NewGuid(), Name = "Test 2", ItemSum = 400, ItemAmount = 20 }
+            };
+
+            conn.Add(
+                samples,
+                (mapper, entity) => {
+                    mapper.Map(entity.Id, NpgsqlDbType.Uuid);
+                    mapper.Map(entity.Name, NpgsqlDbType.Varchar);
+                    mapper.Map(entity.ItemAmount, NpgsqlDbType.Integer);
+                    mapper.Map(entity.ItemSum, NpgsqlDbType.Numeric);
+                },
+                "public",
+                "samples"
+            );
+
+            Console.ReadKey();
         }
     }
 }
--- src/PgPartner/PgPartnerCopyOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PgPartner
-{
-    public class PgPartnerCopyOptions
-    {
-        public bool TableNamesHaveQuotes { get; set; } = false;
-
-        public bool ColumnNamesHaveQuotes { get; set; } = false
-    }
-}
