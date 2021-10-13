4fc05633a39b0aa4b8715839fc1787724d648383
oleg3790 <oleg.krysko@outlook.com>
2021-10-12 23:06:16 -0500

    Add more sample columns

--- src/PgPartner.SampleApp/Models/Sample.cs
+++ src/PgPartner.SampleApp/Models/Sample.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NpgsqlTypes;
+using System;
+using System.Net;
 
 namespace PgPartner.SampleApp.Models
 {
@@ -10,6 +12,16 @@ namespace PgPartner.SampleApp.Models
 
         public int? ItemSum { get; set; }
 
-        public decimal? ItemAmount { get; set;  }
+        public decimal? ItemAmount { get; set; }
+
+        public DateTime? Created { get; set; }
+
+        public DateTimeOffset? CreatedDate { get; set; }
+
+        public byte[] Test1 { get; set; }
+
+        public bool Test2 { get; set; }
+
+        public IPAddress? Test3 { get; set; }
     }
 }
--- src/PgPartner.SampleApp/SampleApp.cs
+++ src/PgPartner.SampleApp/SampleApp.cs
@@ -1,9 +1,12 @@
 ﻿using Npgsql;
+using NpgsqlTypes;
 using PgPartner.SampleApp.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 
 namespace PgPartner.SampleApp
 {
@@ -13,10 +16,15 @@ namespace PgPartner.SampleApp
             create extension if not exists ""uuid-ossp"";
 
             create table public.""Samples"" (
-                id          uuid default uuid_generate_v4(),
-                name        varchar(200) not null,
-                sum         int null,
-                amount      decimal null
+                id              uuid default uuid_generate_v4(),
+                name            varchar(200) not null,
+                sum             int null,
+                amount          decimal null,
+                created         timestamp null,
+                created_date    timestamp with time zone null,
+                test1           bytea null,
+                test2           boolean default false,
+                test3           inet null
             );";
 
         private static string QueryForSamplesTable = @"
@@ -56,7 +64,12 @@ namespace PgPartner.SampleApp
                             Id = new Guid(values[0]),
                             Name = values[1],
                             ItemSum = Convert.ToInt32(string.IsNullOrWhiteSpace(values[2]) ? null : values[2]),
-                            ItemAmount = Convert.ToDecimal(string.IsNullOrWhiteSpace(values[3]) ? null : values[3])
+                            ItemAmount = Convert.ToDecimal(string.IsNullOrWhiteSpace(values[3]) ? null : values[3]),
+                            Created = string.IsNullOrWhiteSpace(values[4]) ? (DateTime?)null : DateTime.Parse(values[4]),
+                            CreatedDate = string.IsNullOrWhiteSpace(values[5]) ? (DateTimeOffset?)null : DateTimeOffset.Parse(values[5]),
+                            Test1 = Encoding.UTF8.GetBytes(values[6]),
+                            Test2 = string.IsNullOrWhiteSpace(values[7]) ? false : bool.Parse(values[7]),
+                            Test3 = string.IsNullOrWhiteSpace(values[8]) ? null : IPAddress.Parse(values[8])
                         };
                     })
                     .ToList(); // Ensures we're returning all results at once and not defering until enumeration is necessary
