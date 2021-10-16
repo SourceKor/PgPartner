02a4725ad3a298aa5298a0220df288db46a9b0c0
oleg3790 <oleg.krysko@outlook.com>
2021-10-16 00:33:34 -0500

    Try adjusting artifacts download path

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,6 +1,8 @@
 NUGET_KEY=$1
 
-cd src/PgPartner/bin/Release
+ls -a
+cd Release
+ls -a
 nugetFilename=$(ls -a | grep '.nupkg$')
 
 dotnet nuget push $nugetFilename --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
