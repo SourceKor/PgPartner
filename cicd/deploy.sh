29a6d7b9bc0b3820b1f613a2a603e767afe16b61
oleg3790 <oleg.krysko@outlook.com>
2021-10-16 00:49:14 -0500

    Skip duplicate deployments

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -5,4 +5,4 @@ cd Release
 ls -a
 nugetFilename=$(ls -a | grep '.nupkg$')
 
-dotnet nuget push $nugetFilename --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push $nugetFilename --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json --skip-duplicate
