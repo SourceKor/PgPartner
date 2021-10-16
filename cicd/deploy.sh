52c3f1aafe08cdafd954b3c2ed3eb3464acd3373
oleg3790 <oleg.krysko@outlook.com>
2021-10-16 00:20:37 -0500

    Fix release path

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,6 +1,6 @@
 NUGET_KEY=$1
 
-cd src/PgPartner/Release
+cd src/PgPartner/bin/Release
 nugetFilename=$(ls -a | grep '.nupkg$')
 
 dotnet nuget push $nugetFilename --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
