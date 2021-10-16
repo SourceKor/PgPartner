a3aa8a6a0693d25cb1d2cfd2cd152356452fd124
oleg3790 <oleg.krysko@outlook.com>
2021-10-16 00:12:23 -0500

    Navigate to release dir

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,7 +1,6 @@
 NUGET_KEY=$1
 
-ls
-pwd
+cd src/PgPartner/Release
 nugetFilename=$(ls -a | grep '.nupkg$')
 
 dotnet nuget push $nugetFilename --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
