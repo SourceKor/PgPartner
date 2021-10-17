193ea7bf1012d4646380cce8e755033fbda1d95c
oleg3790 <oleg.krysko@outlook.com>
2021-10-16 21:56:33 -0500

    Add BulkAdd integration test

---
+++ src/PgPartner.Test/Models/TestItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PgPartner.Test.Models
+{
+    public class TestItem
+    {
+        public Guid Id { get; set; }
+
+        public string Text { get; set; }
+
+        public int Number { get; set; }
+    }
+}
---
+++ src/PgPartner.Test/Utilities/TestCategories.cs
@@ -0,0 +1,8 @@
+﻿namespace PgPartner.Test.Utilities
+{
+    public static class TestCategories
+    {
+        public const string Integration = "Integration";
+        public const string Unit = "Unit";
+    }
+}
