68dae4d29c54724d80c9aafde8938479aab9e6ad
oleg3790 <oleg.krysko@outlook.com>
2021-10-16 21:56:33 -0500

    Add BulkAdd integration test

---
+++ src/PgPartner.Test/BulkAddIntegrationTests.cs
@@ -0,0 +1,86 @@
+using Dapper;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Npgsql;
+using NpgsqlTypes;
+using PgPartner.Test.Models;
+using PgPartner.Test.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PgPartner.Test
+{
+    [TestClass]
+    [TestCategory(TestCategories.Integration)]
+    public class BulkAddIntegrationTests
+    {
+        private static string CreateTestItemsTable = @"
+            create extension if not exists ""uuid-ossp"";
+
+            create table public.""TestItems"" (
+                id              uuid default uuid_generate_v4(),
+                text            varchar(200) not null,
+                number          int null
+            );";
+
+        private static string DropTestItemsTable = @"drop table public.""TestItems"";";
+
+        private NpgsqlConnection _connection;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _connection = new NpgsqlConnection("Server=localhost; Port=5432; Database=test; Username=postgres; Password=admin;");
+            _connection.Open();
+
+            var createTableCommand = new NpgsqlCommand(CreateTestItemsTable, _connection);
+            createTableCommand.ExecuteNonQuery();
+        }
+
+        [TestMethod]
+        public void BulkAdd()
+        {
+            // Arrange
+            var expectedTestItems = new List<TestItem>
+            {
+                new TestItem { Id = Guid.NewGuid(), Text = "Test", Number = 10 },
+                new TestItem { Id = Guid.NewGuid(), Text = "Test2", Number = 20 },
+                new TestItem { Id = Guid.NewGuid(), Text = "Test3", Number = 30 },
+                new TestItem { Id = Guid.NewGuid(), Text = "Test4", Number = 40 }
+            };
+
+            // Act
+            _connection.BulkAdd(
+                expectedTestItems,
+                (mapper, entity) => {
+                    mapper.Map("id", entity.Id, NpgsqlDbType.Uuid);
+                    mapper.Map("text", entity.Text, NpgsqlDbType.Varchar);
+                    mapper.Map("number", entity.Number, NpgsqlDbType.Integer);
+                }, "public", "\"TestItems\"");
+
+            var actualTestItems = _connection.Query<TestItem>("select * from public.\"TestItems\"");
+
+            // Assert
+            actualTestItems.Should().HaveCount(4);
+
+            foreach (var expectedItem in expectedTestItems)
+            {
+                var actualResult = actualTestItems.FirstOrDefault(a => a.Id == expectedItem.Id);
+                actualResult.Should().NotBeNull();
+
+                actualResult.Text.Should().Be(expectedItem.Text);
+                actualResult.Number.Should().Be(expectedItem.Number);
+            }
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            var dropTableCommand = new NpgsqlCommand(DropTestItemsTable, _connection);
+            dropTableCommand.ExecuteNonQuery();
+
+            _connection.Dispose();
+        }
+    }
+}
