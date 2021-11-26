23f962894921fda06da9f410a9fcfa21acc4ada9
oleg3790 <oleg.krysko@outlook.com>
2021-11-25 21:00:05 -0600

    Return table details

---
+++ src/PgPartner/Models/TableDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace PgPartner.Models
+{
+    public class TableDetails
+    {
+        public string SchemaName { get; set; }
+
+        public string TableName { get; set; }
+
+        public string TableType { get; set; }
+    }
+}
